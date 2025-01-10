37a4b27977848dbee941c237961af43a75b109e3
ktourelb <k.toure@lindbaum.de>
2025-01-10 15:19:57 +0100

    zwischenspeicherung

--- settings.php
+++ settings.php
@@ -10,48 +10,98 @@
 
 <?php
 require "functions.php";
+session_start();
+
+$username = $_SESSION['username'] ?? null;
+$password = $_SESSION['password'] ?? null;
+$schoolUrl = $_SESSION['schoolUrl'] ?? null;
 
 $conn = connectToDatabase();
 
-$pushoverApiKey = $_POST['pushoverApiKey'] ?? null;
-$pushoverUserKey = $_POST['pushoverUserKey'] ?? null;
-$schoolUrl = $_POST['schoolUrl'] ?? null;
-$notificationForDaysInAdvance = $_POST['notificationDays'] ?? null;
 
-if ($pushoverApiKey && $pushoverUserKey && $schoolUrl && $notificationForDaysInAdvance) {
-    writeFourArgToDatabase($pushoverApiKey, $pushoverUserKey, $schoolUrl, $notificationForDaysInAdvance, "
-        UPDATE users SET pushover_api_key = ?, pushover_user_key = ?, school_url = ?, notification_for_days_in_advance = ? WHERE username = 'MüllerNik'");
+$pushoverApiKey = getDataWithOneArgFromDatabase($username, "pushover_api_key", "SELECT pushover_api_key FROM users WHERE username = ?");
+$pushoverUserKey = getDataWithOneArgFromDatabase($username, "pushover_user_key", "SELECT pushover_user_key FROM users WHERE username = ?");
+$notificationForDaysInAdvance = getDataWithOneArgFromDatabase($username, "notification_for_days_in_advance", "SELECT notification_for_days_in_advance FROM users WHERE username = ?");
+
+
+//$var = isset($_POST["name"] ) ? $_POST["name"]: '';
+
+//echo isset($_POST['pushoverApiKey']);
+//
+//if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//    if ($_POST['pushoverApiKey'])) {
+//        $pushoverApiKey = $_POST['pushoverApiKey'];
+//        $newPushoverApiKey = true;
+//    }
+//    if (isset($_POST['pushoverUserKey'])) {
+//        $pushoverUserKey = $_POST['pushoverUserKey'];
+//        $newPushoverUserKey = true;
+//    }
+//    $notificationForDaysInAdvance = $_POST['notificationDays'];
+//}
+
+//$pushoverApiKey = $_POST['pushoverApiKey'] == "" ?? getDataWithOneArgFromDatabase($username, "pushover_api_key", "SELECT pushover_api_key FROM users WHERE username = ?");
+//$pushoverUserKey = $_POST['pushoverUserKey'] == "" ?? getDataWithOneArgFromDatabase($username, "pushover_user_key", "SELECT pushover_user_key FROM users WHERE username = ?");
+//$notificationForDaysInAdvance = $_POST['notificationDays'] == "" ?? getDataWithOneArgFromDatabase($username, "notification_for_days_in_advance", "SELECT notification_for_days_in_advance FROM users WHERE username = ?");
+
+if($_POST['pushoverApiKey'] == ""){
+    echo "API Key ist leer";
 }
 
+//    echo "API Key: $pushoverApiKey <br>";
+//    echo "User Key: $pushoverUserKey <br>";
+//    echo "Days: $notificationForDaysInAdvance <br>";
 
+    //if($newPushoverApiKey == true && $newPushoverUserKey == true){
+        writeFourArgToDatabase($pushoverApiKey, $pushoverUserKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_api_key = ?, pushover_user_key = ?, notification_for_days_in_advance = ? WHERE username = ?");
+//        $newPushoverApiKey = false;
+//        $newPushoverUserKey = false;
+//    } elseif ($newPushoverApiKey == true){
+//        writeThreeArgToDatabase($pushoverApiKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_api_key = ?, notification_for_days_in_advance = ? WHERE username = ?");
+//        $newPushoverApiKey = false;
+//    } elseif ($newPushoverUserKey == true){
+//        writeThreeArgToDatabase($pushoverUserKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_user_key = ?, notification_for_days_in_advance = ? WHERE username = ?");
+//        $newPushoverUserKey = false;
+//    } else {
+//        writeTwoArgToDatabase($notificationForDaysInAdvance, $username, "UPDATE users SET notification_for_days_in_advance = ? WHERE username = ?");
+//    }
+//
+//    $conn->close();
+
+
+initiateCheck();
 
 if (isset($_POST['action'])) {
+    $conn = connectToDatabase();
     switch ($_POST['action']) {
         case 'logout':
-            //logout();
-            echo "logout.";
+            logOut();
             break;
         case 'deleteAccount':
-            //deleteAccount();
-            echo "delete acc.";
+            writeOneArgToDatabase($username, "DELETE FROM users WHERE username = ?");
+            echo "Konto gelöscht.";
+            $conn->close();
+            logOut();
             break;
         case 'testNotification':
             sendPushoverNotification("Testbenachrichtigung", "Wenn du das hier ließt, hast du alles richtig gemacht! Ab sofort, erhälst du Benachrichtitigungen, wenn es Änderungen in deinem Stundenplan gibt. Alle 15 Min. wind überprüft, ob Änderungen vorhanden sind.", "");
             break;
     }
-
 }
 
-function logout() {
-    // Deine PHP-Funktion für das Abmelden
-    echo "Abgemeldet.";
+function logOut(){
+    global $conn;
+
+    $conn->close();
+    setcookie(session_name(), '', 100);
+    session_unset();
+    session_destroy();
+    $_SESSION = array();
+    header("Location: startpage.php");
 }
 
-function deleteAccount() {
-    // Deine PHP-Funktion für das Löschen des Kontos
-    echo "Konto gelöscht.";
-}
 ?>
+
 
 
 
@@ -64,30 +114,23 @@ function deleteAccount() {
         <br>
         <label for="pushoverApiKey">Pushover API Key:</label>
         <div class="label-container">
-            <input type="text" id="pushoverApiKey" name="pushoverApiKey" required>
+            <input type="text" id="pushoverApiKey" name="pushoverApiKey" placeholder="<?php echo $pushoverApiKey;?>">
             <span class="info-icon" onclick="openExternInfoSite('ApiKey')">?</span>
         </div>
         <br><br>
 
         <label for="pushoverUserKey">Pushover User Key:</label>
         <div class="label-container">
-            <input type="text" id="pushoverUserKey" name="pushoverUserKey" required>
+            <input type="text" id="pushoverUserKey" name="pushoverUserKey" placeholder="<?php echo $pushoverUserKey;?>">
             <span class="info-icon" onclick="openExternInfoSite('UserKey')">?</span>
         </div>
         <br><br>
 
-        <label for="schoolUrl">Schul-URL:</label>
-        <div class="label-container">
-            <input type="text" id="schoolUrl" name="schoolUrl" placeholder="https://niobe.webuntis.com/WebUntis/jsonrpc.do?school=gym-osterode">
-            <span class="info-icon" onclick="toggleInfo('info-schoolUrl')">?</span>
-        </div>
-        <div class="info-field" id="info-schoolUrl">Hier muss nichts eingegeben werden, wenn du auf dem TRG bist.</div>
-        <br><br>
 
         <label for="notificationDays">Für wie viele Tage im Voraus möchtest du benachrichtigt werden, wenn Änderungen auftreten?</label>
         <div class="label-container">
-            <input type="range" id="notificationDays" name="notificationDays" min="1" max="30" value="14" oninput="this.nextElementSibling.value = this.value">
-            <output>14</output>
+            <input type="range" id="notificationDays" name="notificationDays" min="1" max="30" value="<?php echo $notificationForDaysInAdvance;?>" oninput="this.nextElementSibling.value = this.value">
+            <output><?php echo $notificationForDaysInAdvance;?></output>
             <span class="info-icon" onclick="openExternInfoSite('TageInVorraus')">?</span>
         </div>
         <br><br>
@@ -103,47 +146,6 @@ function deleteAccount() {
     </form>
 
 </div>
-<script>
-    function toggleInfo(id) {
-        var infoField = document.getElementById(id);
-        infoField.style.display = (infoField.style.display === "block") ? "none" : "block";
-    }
-
-    document.addEventListener('click', function(event) {
-        var infoFields = document.querySelectorAll('.info-field');
-        infoFields.forEach(function(infoField) {
-            if (!infoField.contains(event.target) && !event.target.classList.contains('info-icon')) {
-                infoField.style.display = 'none';
-            }
-        });
-    });
-
-    function openExternInfoSite(info) {
-        var url = "";
-        switch(info) {
-            case "ApiKey":
-                url = "https://pushover.net/apps/build";
-                break;
-            case "UserKey":
-                url = "https://pushover.net/apps/build";
-                break;
-            case "TageInVorraus":
-                url = "https://niklas.craft.me/Untis-Benachrichtigungen-Anleitung/b/594ADF73-6DE4-40AB-9B9A-A668C4A53721/F%C3%BCr-wie-viele-Tage-im-Voraus-m%C3%B6chtes";
-                break;
-
-
-        }
-        window.open(url, '_blank');
-    }
-
-    document.querySelector('form').addEventListener('submit', function(event) {
-        var schoolUrlInput = document.getElementById('schoolUrl');
-        if (schoolUrlInput.value.trim() === '') {
-            schoolUrlInput.value = schoolUrlInput.placeholder;
-        }
-    });
-
-
-</script>
 </body>
+<script src="script.js"></script>
 </html>
