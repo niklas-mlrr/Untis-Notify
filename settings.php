0da83de756cb6ac6f12cd7e7b61071796cd867da
TechNikFuture <tech.nik.effects@gmail.com>
2025-01-14 15:56:49 +0100

    Switched notification-service to Slack

--- settings.php
+++ settings.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="de">
-
 <head>
-    <meta charset="UTF-8">
     <title>Einstellungen</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -21,62 +20,36 @@ $schoolUrl = $_SESSION['schoolUrl'] ?? null;
 $conn = connectToDatabase();
 
 
-$pushoverApiKey = getDataWithOneArgFromDatabase($username, "pushover_api_key", "SELECT pushover_api_key FROM users WHERE username = ?");
-$pushoverUserKey = getDataWithOneArgFromDatabase($username, "pushover_user_key", "SELECT pushover_user_key FROM users WHERE username = ?");
+$slackBotToken = getDataWithOneArgFromDatabase($username, "slack_bot_token", "SELECT slack_bot_token FROM users WHERE username = ?");
 $notificationForDaysInAdvance = getDataWithOneArgFromDatabase($username, "notification_for_days_in_advance", "SELECT notification_for_days_in_advance FROM users WHERE username = ?");
 
 
 
-$newPushoverApiKey = false;
-$newPushoverUserKey = false;
-if(isset($_POST['pushoverApiKey'])) {
-    if ($_POST['pushoverApiKey'] != "") {
-        $pushoverApiKey = $_POST['pushoverApiKey'];
-        $newPushoverApiKey = true;
+$newSlackBotToken = false;
+if(!empty($_POST['slackBotToken'])) {
+        $slackBotToken = $_POST['slackBotToken'];
+        $newSlackBotToken = true;
+    }
+
+$notificationForDaysInAdvance = $_POST['notificationDays'] ?? $notificationForDaysInAdvance;
+
+
+if($newSlackBotToken){
+    if(writeThreeArgToDatabase($slackBotToken, $notificationForDaysInAdvance, $username, "UPDATE users SET slack_bot_token = ?, notification_for_days_in_advance = ? WHERE username = ?")){
+        $btnResponse = getBtnText(true);
+    } else {
+        $btnResponse = getBtnText(false);
+    }
+    $newSlackBotToken = false;
+} elseif ($notificationForDaysInAdvance){
+    if(writeTwoArgToDatabase(false, $notificationForDaysInAdvance, $username, "UPDATE users SET notification_for_days_in_advance = ? WHERE username = ?")){
+        $btnResponse = getBtnText(true);
+    } else {
+        $btnResponse = getBtnText(false);
     }
 }
-if(isset($_POST['pushoverUserKey'])) {
-    if ($_POST['pushoverUserKey'] != "") {
-        $pushoverUserKey = $_POST['pushoverUserKey'];
-        $newPushoverUserKey = true;
-    }
-    $notificationForDaysInAdvance = $_POST['notificationDays'];
-}
-
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if($newPushoverApiKey && $newPushoverUserKey){
-        if(writeFourArgToDatabase($pushoverApiKey, $pushoverUserKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_api_key = ?, pushover_user_key = ?, notification_for_days_in_advance = ? WHERE username = ?")){
-            $btnResponse = '<p class="sucessful">Einstellungen erfolgreich gespeichert</p>';
-        } else {
-            $btnResponse = '<p class="failed">Fehler beim Speichern der Einstellungen</p>';
-        }
-        $newPushoverApiKey = false;
-        $newPushoverUserKey = false;
-    } elseif ($newPushoverApiKey){
-        if(writeThreeArgToDatabase($pushoverApiKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_api_key = ?, notification_for_days_in_advance = ? WHERE username = ?")){
-            $btnResponse = '<p class="sucessful">Einstellungen erfolgreich gespeichert</p>';
-        } else {
-            $btnResponse = '<p class="failed">Fehler beim Speichern der Einstellungen</p>';
-        }
-        $newPushoverApiKey = false;
-    } elseif ($newPushoverUserKey){
-        if(writeThreeArgToDatabase($pushoverUserKey, $notificationForDaysInAdvance, $username, "UPDATE users SET pushover_user_key = ?, notification_for_days_in_advance = ? WHERE username = ?")){
-            $btnResponse = '<p class="sucessful">Einstellungen erfolgreich gespeichert</p>';
-        } else {
-            $btnResponse = '<p class="failed">Fehler beim Speichern der Einstellungen</p>';
-        }
-        $newPushoverUserKey = false;
-    } elseif ($notificationForDaysInAdvance){
-        if(writeTwoArgToDatabase($notificationForDaysInAdvance, $username, "UPDATE users SET notification_for_days_in_advance = ? WHERE username = ?")){
-            $btnResponse = '<p class="sucessful">Einstellungen erfolgreich gespeichert</p>';
-        } else {
-            $btnResponse = '<p class="failed">Fehler beim Speichern der Einstellungen</p>';
-        }
-    }
 $conn->close();
-}
+
 
 
 
@@ -90,7 +63,7 @@ if (isset($_POST['action'])) {
             break;
         case 'deleteAccount':
             if(!writeOneArgToDatabase($username, "DELETE FROM users WHERE username = ?")){
-                $btnResponse = '<p class="sucessful">Konto erfolgreich gelöscht</p>';
+                $btnResponse = '<p class="successful">Konto erfolgreich gelöscht</p>';
                 sleep(2);
                 $conn->close();
                 logOut();
@@ -99,8 +72,10 @@ if (isset($_POST['action'])) {
             }
             break;
         case 'testNotification':
-            if(sendPushoverNotification("Testbenachrichtigung", "Wenn du das hier ließt, hast du alles richtig gemacht! Ab sofort, erhälst du Benachrichtitigungen, wenn es Änderungen in deinem Stundenplan gibt. Alle 15 Min. wind überprüft, ob Änderungen vorhanden sind.", "")){
-                $btnResponse = '<p class="sucessful">Testbenachrichtigung erfolgreich gesendet</p>';
+            if(sendslackMessage("Testbenachrichtigung", "Wenn du das hier ließt, hast du alles richtig gemacht! Ab sofort, erhälst du Benachrichtitigungen, wenn es Änderungen in deinem Stundenplan gibt. Alle 15 Min. wind überprüft, ob Änderungen vorhanden sind.", "")){
+                if(writeOneArgToDatabase($username, "UPDATE users SET setup_complete = true WHERE username = ?")){
+                    $btnResponse = '<p class="successful">Testbenachrichtigung erfolgreich gesendet</p>';
+                }
             } else {
                 $btnResponse = '<p class="failed">Fehler beim Senden der Testbenachrichtigung</p>';
             }
@@ -108,13 +83,6 @@ if (isset($_POST['action'])) {
     }
 }
 
-function logOut(){
-    setcookie(session_name(), '', 100);
-    session_unset();
-    session_destroy();
-    $_SESSION = array();
-    header("Location: index.php");
-}
 
 ?>
 
@@ -126,17 +94,10 @@ function logOut(){
     <form action="settings.php" method="post">
         <h2>Einstellungen</h2>
         <br>
-        <label for="pushoverApiKey">Pushover API Key:</label>
+        <label for="slackBotToken">Slack Bot Token:</label>
         <div class="label-container">
-            <input type="text" id="pushoverApiKey" name="pushoverApiKey" placeholder="<?php echo $pushoverApiKey;?>">
-            <span class="info-icon" onclick="openExternInfoSite('ApiKey')">?</span>
-        </div>
-        <br><br>
-
-        <label for="pushoverUserKey">Pushover User Key:</label>
-        <div class="label-container">
-            <input type="text" id="pushoverUserKey" name="pushoverUserKey" placeholder="<?php echo $pushoverUserKey;?>">
-            <span class="info-icon" onclick="openExternInfoSite('UserKey')">?</span>
+            <input type="text" id="slackBotToken" name="slackBotToken" placeholder="<?php echo $slackBotToken;?>">
+            <span class="info-icon" onclick="openExternInfoSite('BotToken')">?</span>
         </div>
         <br><br>
 
