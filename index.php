8d7f083129573ab154bb1b60d2788b60c4fb2fc2
Tech Nik <tech.nik.effects@gmail.com>
2025-01-26 16:12:08 +0100

    Testing GitHub Webhook

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <title>Untis Notify</title>
+    <title>test Notify</title>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="apple-touch-icon" href="logo.svg">
     <link rel="icon" href="logo.svg" type="image/svg+xml">
