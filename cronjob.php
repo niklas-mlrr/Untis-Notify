93d18eeadf46e00f26c5d0ea40dd3d08b0347cb1
Tech Nik <tech.nik.effects@gmail.com>
2025-01-27 20:49:20 +0100

    Cronjob Bug Test (Final)

--- cronjob.php
+++ cronjob.php
@@ -1,16 +1,9 @@
 <?php
 
 
-$serverName = $_SERVER['SERVER_NAME'] ?? '';
-$agentIsNotCronjob = $_SERVER['HTTP_HOST'] ?? false;
 
-if ($serverName === 'localhost' || $agentIsNotCronjob) {
-    $baseDir = __DIR__;
-} else {
-    $baseDir = __DIR__ . "/domains/untis-notify.de/public_html";
-}
 
-require_once $baseDir . "/functions.php";
+require_once __DIR__ . "/functions.php";
 
 use Exceptions\DatabaseException;
 
